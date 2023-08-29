dedf44af7f622d83ed267377cb6480c9b353fc14
Andrea Canale <vivanco11.ac@gmail.com>
2023-08-29 19:00:48 +0200

    Fix MQTT SSL handshake

--- components/mqtt_helper/mqtt_helper.c
+++ components/mqtt_helper/mqtt_helper.c
@@ -1,5 +1,6 @@
 #include "mqtt_client.h"
 #include "secrets.h"
+#include "esp_log.h"
 
 #define TAG "MQTT"
 #define SUB "esp8266/sub"
@@ -11,25 +12,51 @@ int subscribed = 0;
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
     esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    // your_context_t *context = event->context;
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
-            esp_mqtt_client_subscribe(client, SUB, 0);
+            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+            msg_id = esp_mqtt_client_subscribe(client, "sdk/test/python", 0);
+            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+            // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            // msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+            // ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_DISCONNECTED:
             subscribed = 0;
+            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
             break;
+
         case MQTT_EVENT_SUBSCRIBED:
             subscribed = 1;
+            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+            msg_id = esp_mqtt_client_publish(client, "sdk/test/python", "connected", 0, 0, 0);
+            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_UNSUBSCRIBED:
             subscribed = 0;
+            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
             break;
         case MQTT_EVENT_ERROR:
+            ESP_LOGE(TAG, "MQTT_EVENT_ERROR");
+                ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
             break;
         default:
+            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
             break;
     }
-    
     return ESP_OK;
 }
 
@@ -40,7 +67,7 @@ esp_mqtt_client_handle_t mqtt_app_start(void)
         .event_handle = mqtt_event_handler,
         .client_cert_pem = client_cert,
         .client_key_pem = privkey,
-        .client_id = "ESP8266_Wemos",
+        .client_id = "basicPubSub"
     };
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_start(client);
